aca2320fae55f08d6db16e13b64b085d17a2cf29
Ihor Krykunov <4430704@gmail.com>
2025-07-28 19:34:24 +0300

    Add configuration files and styling

---
+++ spotify-widget/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['i.scdn.co'], // Spotify image domain
+  },
+}
+
+module.exports = nextConfig
